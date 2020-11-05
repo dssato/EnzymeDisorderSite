1e1d3f7a179ad06f6f5f105344756c404f006747
dssato <73856135+dssato@users.noreply.github.com>
2020-11-04 18:03:49 -0800

    improvements and added pages

--- background.js
+++ background.js
@@ -1,2 +1,12 @@
-var c = document.getElementById("background");
-var ctx = c.getContext("2d");
+document.addEventListener("scroll", e => {
+    let scroll = window.scrollY;
+    let navbar = document.getElementById("topnav");
+    let content = document.getElementById("content");
+    if(scroll > 344) {
+        navbar.classList.add("navbar_main_fixed")
+        content.classList.add("content_scrolled")
+    } else {
+        navbar.classList.remove("navbar_main_fixed")
+        content.classList.remove("content_scrolled")
+    }
+})
